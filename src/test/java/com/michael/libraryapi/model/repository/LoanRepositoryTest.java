7f29080a2ee5753c5950f7dbf2eed5385037afec
Michael Aguiar <michael.aguiar@cs.inf.br>
2021-02-17 01:29:09 -0400

    LoanRepositoryTest

---
+++ src/test/java/com/michael/libraryapi/model/repository/LoanRepositoryTest.java
@@ -0,0 +1,53 @@
+package com.michael.libraryapi.model.repository;
+
+import com.michael.libraryapi.model.entity.Book;
+import com.michael.libraryapi.model.entity.Loan;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDate;
+
+import static com.michael.libraryapi.model.repository.BookRepositoryTest.createNewBook;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@DataJpaTest
+public class LoanRepositoryTest {
+
+    @Autowired
+    private LoanRepository repository;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Test
+    @DisplayName("deve verificar se existe empréstimo não devolvido para o livro.")
+    public void existsByBookAndNotReturnedTest(){
+        //cenário
+        Loan loan = createAndPersistLoan(LocalDate.now());
+        Book book = loan.getBook();
+
+        //execucao
+        boolean exists = repository.existsByBookAndNotReturned(book);
+
+        assertThat(exists).isTrue();
+    }
+
+    public Loan createAndPersistLoan(LocalDate loanDate){
+        Book book = createNewBook("123");
+        entityManager.persist(book);
+
+        Loan loan = Loan.builder().book(book).customer("Fulano").loanDate(loanDate).build();
+        entityManager.persist(loan);
+
+        return loan;
+    }
+
+}
